1961ba356d3aacbb323c30ecd3e671af4a35c46a
JDR23 <juandavidrestrepo30@gmail.com>
2025-09-22 23:13:43 -0500

    feat:cambios

--- crud/crud_usuario.py
+++ crud/crud_usuario.py
@@ -1,13 +1,12 @@
 from sqlalchemy.orm import Session
-from entities.usuario import Usuario
+from models.usuario import Usuario
 from schemas import UsuarioCreate, UsuarioUpdate
-import uuid
+from models import Usuario
 
 
 # Crear usuario
-def create_usuario(db: Session, usuario: UsuarioCreate):
-    db_usuario = Usuario(
-        id_usuario=uuid.uuid4(),
+def crear_usuario(db: Session, usuario: UsuarioCreate):
+    nuevo_usuario = Usuario(
         primer_nombre_usuario=usuario.primer_nombre_usuario,
         segundo_nombre_usuario=usuario.segundo_nombre_usuario,
         primer_apellido_usuario=usuario.primer_apellido_usuario,
@@ -15,29 +14,30 @@ def create_usuario(db: Session, usuario: UsuarioCreate):
         rol_usuario=usuario.rol_usuario,
         fecha_nacimiento_usuario=usuario.fecha_nacimiento_usuario,
     )
-    db.add(db_usuario)
+    db.add(nuevo_usuario)
     db.commit()
-    db.refresh(db_usuario)
-    return db_usuario
+    db.refresh(nuevo_usuario)
+    return nuevo_usuario
 
 
-# Obtener todos los usuarios
-def get_usuarios(db: Session, skip: int = 0, limit: int = 10):
+# Listar usuarios
+def obtener_usuarios(db: Session, skip: int = 0, limit: int = 10):
     return db.query(Usuario).offset(skip).limit(limit).all()
 
 
-# Obtener un usuario por ID
-def get_usuario_by_id(db: Session, usuario_id: uuid.UUID):
-    return db.query(Usuario).filter(Usuario.id_usuario == usuario_id).first()
+# Obtener usuario por ID
+def obtener_usuario_por_id(db: Session, usuario_id: str):
+    return db.query(Usuario).filter(Usuario.id == usuario_id).first()
 
 
 # Actualizar usuario
-def update_usuario(db: Session, usuario_id: uuid.UUID, usuario: UsuarioUpdate):
-    db_usuario = db.query(Usuario).filter(Usuario.id_usuario == usuario_id).first()
+def actualizar_usuario(db: Session, usuario_id: str, usuario: UsuarioUpdate):
+    db_usuario = obtener_usuario_por_id(db, usuario_id)
     if not db_usuario:
         return None
 
-    for key, value in usuario.dict(exclude_unset=True).items():
+    update_data = usuario.dict(exclude_unset=True)
+    for key, value in update_data.items():
         setattr(db_usuario, key, value)
 
     db.commit()
@@ -46,10 +46,11 @@ def update_usuario(db: Session, usuario_id: uuid.UUID, usuario: UsuarioUpdate):
 
 
 # Eliminar usuario
-def delete_usuario(db: Session, usuario_id: uuid.UUID):
-    db_usuario = db.query(Usuario).filter(Usuario.id_usuario == usuario_id).first()
+def eliminar_usuario(db: Session, usuario_id: str):
+    db_usuario = obtener_usuario_por_id(db, usuario_id)
     if not db_usuario:
         return None
+
     db.delete(db_usuario)
     db.commit()
     return db_usuario
