8bf6c3815dc2082eddb6fba921fd8cb2f23b31e3
JDR23 <juandavidrestrepo30@gmail.com>
2025-09-22 22:07:43 -0500

    feat:actualizacion-del-codigo

---
+++ db_conn.py
@@ -0,0 +1,20 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+# Cargar variables desde .env
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+try:
+    conn = psycopg2.connect(DATABASE_URL)
+    cur = conn.cursor()
+    cur.execute("SELECT version();")
+    record = cur.fetchone()
+    print("✅ Conectado a:", record)
+    cur.close()
+    conn.close()
+except Exception as e:
+    print("❌ Error de conexión:", e)
+
