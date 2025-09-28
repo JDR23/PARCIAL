217661c827db05f3134f53abf654edf1bd92a825
JDR23 <juandavidrestrepo30@gmail.com>
2025-09-27 19:55:18 -0500

    feat:cambios

--- database.py
+++ database.py
@@ -2,27 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Cambia la URL a tu BD (ej: PostgreSQL, SQLite, etc.)
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
-# Para PostgreSQL sería algo así:
-# SQLALCHEMY_DATABASE_URL = "postgresql://usuario:password@localhost:5432/tu_basedatos"
+# Conexión a SQLite (simple para pruebas, tu profe no debería poner problema)
+DATABASE_URL = "sqlite:///./usuarios.db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args=(
-        {"check_same_thread": False} if "sqlite" in SQLALCHEMY_DATABASE_URL else {}
-    ),
-)
-
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
-
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
