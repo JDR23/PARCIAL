b857ab10986c12c65420525557fec4513fe3b135
JDR23 <juandavidrestrepo30@gmail.com>
2025-11-11 19:13:53 -0500

    feat:cambio

--- models/usuario.py
+++ models/usuario.py
@@ -1,12 +1,13 @@
-from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy import Column, String
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
 from database import Base
-
 
 class Usuario(Base):
     __tablename__ = "usuarios"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     nombre = Column(String, nullable=False)
-    apellido = Column(String, nullable=False)
+    correo = Column(String, nullable=False, unique=True)
+    contrasena = Column(String, nullable=False)
     rol = Column(String, nullable=False)
-    fecha_nacimiento = Column(Date, nullable=False)
